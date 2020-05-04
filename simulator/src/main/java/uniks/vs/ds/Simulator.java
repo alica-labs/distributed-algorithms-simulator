dbe6154bb4fa9cc29c15a6620d98e54886fccd60
Alex Jahl <ajahl.se@gmail.com>
2020-05-04 14:04:46 +0200

    modify main class

--- simulator/src/main/java/uniks/vs/ds/Simulator.java
+++ simulator/src/main/java/uniks/vs/ds/Simulator.java
@@ -26,27 +26,31 @@
 
 package uniks.vs.ds;
 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import uniks.vs.ds.algorithms.test.TestSimNode;
 import uniks.vs.ds.model.NodeTypes;
 import uniks.vs.ds.view.SimulationWindow;
+
 
 /**
  * the main class
  *
  * @author Thomas Weise
  */
-public class Simulator {
+public class Simulator extends Application {
 
-  /**
-   * the main program called at startup
-   *
-   * @param args
-   *          the command line arguments
-   */
-  @SuppressWarnings("unchecked")
-  public static void main(String[] args) {
+    @Override
+    public void start(Stage stage) throws Exception {
 
-    SimulationWindow w = new SimulationWindow();
-
-    w.setVisible(true);
-  }
+      NodeTypes.registerNodeType(TestSimNode.class);
+      Group root = new Group();
+      SimulationWindow simulationWindow = new SimulationWindow(stage, root);
+      stage.setScene(simulationWindow);
+      stage.show();
+    }
 }
